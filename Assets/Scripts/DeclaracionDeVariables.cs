515803dc78dce0e218a1dca2c040f10be99a6c04
Federico Ugarte <coolfede97@gmail.com>
2025-03-17 09:05:35 -0300

    tarea completada

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class DeclaracionDeVariables : MonoBehaviour
 {
     [SerializeField] int gamesWon;
+    [SerializeField] float winRate;
     [SerializeField] string botName;
     [SerializeField] bool usesMiniMax;
     void Start()
     {
+        gamesWon = 11;
+        winRate = 77.4f;
+        botName = "Brokenice";
+        usesMiniMax = true;
         Debug.Log(gamesWon);
+        Debug.Log(winRate);
         Debug.Log(botName);
         Debug.Log(usesMiniMax);
     }
